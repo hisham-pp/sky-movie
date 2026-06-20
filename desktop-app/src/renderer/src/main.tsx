e409b918a1aff7371afdf10c8f424bbbd3b23dff
Hisham <hishammuhammed.in@gmail.com>
2026-06-20 19:55:46 +0530

    feat: add application layout styles and initialize React renderer entry point

--- desktop-app/src/renderer/src/main.tsx
+++ desktop-app/src/renderer/src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import './styles.css';
+import './styles/themes.css';
+import './styles/main.css';
+import './styles/search-modal.css';
+import './styles/unrecognized-drawer.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
